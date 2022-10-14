27636f8365065e855a561750cda1ac95def85023
kurmanbek89 <kumakary2@gmail.com>
2022-10-14 03:02:34 +0300

    first

---
+++ src/Circle.java
@@ -0,0 +1,11 @@
+public class Circle {
+    static final double PI = 3.14d;
+
+
+    public static double area(double a){
+         return PI*(a*a);
+    }
+    public static double circunference(double a){
+        return PI*2*a;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Circle.area(25.5));
+        System.out.println(Circle.circunference(25.5));
+    }
+}
